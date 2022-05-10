c83e2afb0cef1b223edfd82bb333a6a684e671d3
adan <darktyler1122@gmail.com>
2022-05-10 01:31:45 -0400

    se agrega la app

---
+++ src/preguntas.js
@@ -0,0 +1,40 @@
+const preguntas = [
+    {
+        titulo: "¿Cual es el mejor leguaje de programacion?",
+        opciones: [
+            {textoRespuesta: "JavaScript", isCorrect: true},
+            {textoRespuesta: "PHP", isCorrect: false},
+            {textoRespuesta: "C++", isCorrect: false},
+            {textoRespuesta: "Kotlin", isCorrect: false},
+        ],
+    },
+    {
+        titulo: "¿Quien es el mejor instructor de programacion?",
+        opciones: [
+            {textoRespuesta: "jonMircha", isCorrect:true},
+            {textoRespuesta: "SoyDalto", isCorrect:false},
+            {textoRespuesta: "Victor Robles Web", isCorrect:false},
+            {textoRespuesta: "Carlos Azaustre", isCorrect:false},
+        ],
+    },
+    {
+        titulo: "¿cuanto es '11' + 1 en Javascript?",
+        opciones: [
+            {textoRespuesta: "111", isCorrect: true},
+            {textoRespuesta: "12", isCorrect: false},
+            {textoRespuesta: "112", isCorrect: false},
+            {textoRespuesta: "Syntax Error", isCorrect: false},
+        ],
+    },
+    {
+        titulo: "¿En que año fue creado JavaScript?",
+        opciones: [
+            {textoRespuesta: 1997, isCorrect: false},
+            {textoRespuesta: 2001, isCorrect: false},
+            {textoRespuesta: 1987, isCorrect: false},
+            {textoRespuesta: 1995, isCorrect: true},
+        ],
+    }
+];
+
+export default preguntas;
